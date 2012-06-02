4c1d75ffed73c34067ed6b81dde8e6c38cd8febe
kwkoch <korywkoch@gmail.com>
2012-06-02 13:08:09 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KorykoCh::Application.config.secret_token = 'b8f9412790244f82d32f13e000e696414f7b82e2620a031bb111b87b4bbcfe9c2188526f3fbcd423c66637686a7c312303f66c98259b8b7ebe095dd85d9df082'
