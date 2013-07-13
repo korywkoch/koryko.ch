0efc52c4d4b43d7e497dcb87b67c1ef0cf310c01
korywkoch <korywkoch@gmail.com>
2013-07-13 10:13:32 -0400

    Stuff?

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,11 @@
 module ApplicationHelper
   
     def full_title(title)
-      site_name = "koryko.ch"
-      
+      @site_name = "koryko.ch"
       if title.empty?
-        site_name
+        @site_name
       else
-        site_name + " | " + title
+        @site_name + " | " + title
       end
     end
     
